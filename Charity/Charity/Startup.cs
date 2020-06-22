e5b61a6ea93cdad6836ab352031473abfdf5f6b4
asiutra <adamsiutra@onet.pl>
2020-06-22 17:30:56 +0200

    Add all needed nugets, frontend pages and configure startup

--- Charity/Charity/Startup.cs
+++ Charity/Charity/Startup.cs
@@ -16,6 +16,8 @@ namespace Charity
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,16 @@ namespace Charity
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
