63cfaa7a2ea7c6b092d5c384fa71495e0ddea6b6
asiutra <adamsiutra@onet.pl>
2020-06-24 18:44:02 +0200

    Handle Sum Of Quantity in index page

--- Charity/Charity/Controllers/HomeController.cs
+++ Charity/Charity/Controllers/HomeController.cs
@@ -13,14 +13,19 @@ namespace Charity.Controllers
     public class HomeController : Controller
     {
         private readonly IInstitutionService _institutionService;
+        private readonly IDonationService _donationService;
 
-        public HomeController(IInstitutionService institutionService)
+        public HomeController(IInstitutionService institutionService, IDonationService donationService)
         {
             _institutionService = institutionService;
+            _donationService = donationService;
         }
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            var count = await _donationService.GetAllAsync();
+            ViewBag.Quantity = count;
+
             var institutions = await _institutionService.GetAllAsync();
             return View(institutions);
         }
