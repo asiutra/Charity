a7761befaaf001a7fde17a285dd78de6b2d3d467
asiutra <adamsiutra@onet.pl>
2020-06-26 15:10:40 +0200

    Handle Form for donation

--- Charity/Charity/Controllers/DonationController.cs
+++ Charity/Charity/Controllers/DonationController.cs
@@ -2,16 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Charity.Models.Db;
+using Charity.Models.ViewModel;
+using Charity.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Charity.Controllers
 {
     public class DonationController : Controller
     {
+        private readonly ICategoryService _categoryService;
+        private readonly IInstitutionService _institutionService;
+
+        public DonationController(ICategoryService categoryService, IInstitutionService institutionService)
+        {
+            this._categoryService = categoryService;
+            this._institutionService = institutionService;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Donate()
         {
-            return View();
+            var ViewModel = new DonationViewModel();
+
+            ViewModel.CategoryItems = new List<SelectListItem>()
+            {
+                new SelectListItem{Value = _categoryService.GetAsync(1).Result.ID.ToString(), Text = _categoryService.GetAsync(1).Result.Name},
+                new SelectListItem{Value = _categoryService.GetAsync(2).Result.ID.ToString(), Text = _categoryService.GetAsync(2).Result.Name},
+                new SelectListItem{Value = _categoryService.GetAsync(3).Result.ID.ToString(), Text = _categoryService.GetAsync(3).Result.Name},
+                new SelectListItem{Value = _categoryService.GetAsync(4).Result.ID.ToString(), Text = _categoryService.GetAsync(4).Result.Name},
+                new SelectListItem{Value = _categoryService.GetAsync(5).Result.ID.ToString(), Text = _categoryService.GetAsync(5).Result.Name}
+            };
+
+            ViewModel.Institutions = await _institutionService.GetAllAsync();
+            
+            
+            return View(ViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Donate(DonationViewModel viewModel)
+        {
+            return Content("SomeValue");
+            return RedirectToAction("", ""); //redirect to view of confirmation with collected data;
         }
 
     }
---
+++ Charity/Charity/Models/ViewModel/DonationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Charity.Models.Db;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Charity.Models.ViewModel
+{
+    public class DonationViewModel
+    {
+        public IList<Category> Categories;
+        public IList<SelectListItem> CategoryItems { get; set; }
+        public IList<Institution> Institutions;
+        public int QuantityBag;
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime PickUpDate { get; set; }
+        public DateTime PickUpTime { get; set; }
+        public string PickUpComment { get; set; }
+    }
+}
