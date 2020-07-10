111184bbca98881b9339671345c270101f096e59
asiutra <adamsiutra@onet.pl>
2020-07-10 19:28:20 +0200

    Handle Register

---
+++ Charity/Charity/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Charity.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charity.Controllers
+{
+    public class AccountController : Controller
+    {
+        protected UserManager<IdentityUser> UserManager { get; }
+        protected SignInManager<IdentityUser> SignInManager { get; }
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            var user = new IdentityUser
+            {
+                Email = viewModel.Email,
+                UserName = viewModel.Email
+            };
+
+            var result = await UserManager.CreateAsync(user, viewModel.Password);
+
+            if (result.Succeeded)
+            {
+                await SignInManager.SignInAsync(user, false);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await SignInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
