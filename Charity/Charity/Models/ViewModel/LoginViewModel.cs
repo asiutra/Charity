04e545c97b3ea63dbdad3c3c9bea3aea2592ce00
asiutra <adamsiutra@onet.pl>
2020-07-10 22:39:38 +0200

    Handle Login

---
+++ Charity/Charity/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charity.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Wprowadź adres email"), EmailAddress(ErrorMessage = "Niepoprawny adres email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Wprowadź hasło")]
+        public string Password { get; set; }
+    }
+}
