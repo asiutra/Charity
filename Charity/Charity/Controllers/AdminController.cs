2fc449999239447315ce191dd34501a43be9fb68
asiutra <adamsiutra@onet.pl>
2020-07-11 14:27:24 +0200

    HandleForAdmin

---
+++ Charity/Charity/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charity.Controllers
+{
+    //Controller available only for Admin
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
