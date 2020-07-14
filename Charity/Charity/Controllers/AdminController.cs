cba460467b91043265be5909fda8c59429da2ffb
asiutra <adamsiutra@onet.pl>
2020-07-14 10:24:14 +0200

    CreateAdminController_AddBootstrap

--- Charity/Charity/Controllers/AdminController.cs
+++ Charity/Charity/Controllers/AdminController.cs
@@ -2,19 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Charity.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Charity.Controllers
 {
-    //Controller available only for Admin
+    // Controller will be for lock/unlock/show our users
+    // Controller available only for Admin
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        [HttpGet]
-        public IActionResult Dashboard()
+        protected UserManager<IdentityUser> UserManager { get; }
+        protected IAdminService AdminService;
+
+        public AdminController(UserManager<IdentityUser> userManager, IAdminService adminService)
         {
-            return View();
+            UserManager = userManager;
+            AdminService = adminService;
+        }
+
+
+        //[HttpGet]
+        public async Task<IActionResult> ShowUsers()
+        {
+            var users = await UserManager.GetUsersInRoleAsync("User");
+            return View(users);
+        }
+
+        public async Task<IActionResult> Lock(string id)
+        {
+            await AdminService.LockUser(id);
+            return RedirectToAction("ShowUsers");
+        }
+
+        public async Task<IActionResult> Unlock(string id)
+        {
+            await AdminService.UnlockUser(id);
+            return RedirectToAction("ShowUsers");
         }
     }
 }
---
+++ Charity/Charity/Services/Interfaces/IAdminService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charity.Services.Interfaces
+{
+    public interface IAdminService
+    {
+        Task<bool> LockUser(string id);
+        Task<bool> UnlockUser(string id);
+    }
+}
